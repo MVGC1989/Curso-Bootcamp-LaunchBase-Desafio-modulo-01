b3ce65f2a2eb9e9a1546e597c4ddc6b56d785da1
Marcos Carvalho <63380367+MVGC1989@users.noreply.github.com>
2021-01-06 18:08:32 -0300

    Update dadosprogramador.js

--- Desafios/dadosprogramador.js
+++ Desafios/dadosprogramador.js
@@ -1,3 +1,25 @@
+/*Vetores e objetos
+Crie um programa com um objeto para armazenar dados de um programador como nome, idade e tecnologias que trabalha.
+
+Um programador pode trabalhar com várias tecnologias, por isso armazene essas tecnologias em um array.
+
+As tecnologias também devem ser objetos contendo nome e especialidade, use as tecnologias abaixo:
+
+{ nome: 'C++', especialidade: 'Desktop' }
+{ nome: 'Python', especialidade: 'Data Science' }
+{ nome: 'JavaScript', especialidade: 'Web/Mobile' }
+Por exemplo:
+
+const objeto = {
+  propriedade: [
+    { nome: "C++", especialidade: "Desktop" },
+    { nome: "JavaScript", especialidade: "Web/Mobile" }
+  ]
+};
+Imprima em tela o nome e especialidade da primeira tecnologia que o usuário utiliza no seguinte formato:
+
+O usuário Carlos tem 32 anos e usa a tecnologia C++ com especialidade em Desktop*/
+
 const programador = {
     nome: 'Marcos',
     idade: 30,
