dc97fa390ee3b6f55d781ab4cebcab9a832d34fd
Marcos Carvalho <63380367+MVGC1989@users.noreply.github.com>
2021-01-06 18:06:40 -0300

    Update empresa.js

--- Desafios/empresa.js
+++ Desafios/empresa.js
@@ -1,3 +1,21 @@
+/*Construção e impressão de objetos
+Crie um programa que armazena dados da empresa Rocketseat dentro de um objeto chamado empresa. Os dados a serem armazenados são:
+
+Nome: Rocketseat
+
+Cor: Roxo
+
+Foco: Programação
+
+Endereço:
+
+Rua: Rua Guilherme Gembala
+Número: 260
+
+Imprima em tela utilizando console.log o nome da empresa e seu endereço no seguinte formato:
+
+A empresa Rocketseat está localizada em Rua Guilherme Gembala, 260*/
+
 const usuario ={
     nome: 'João',
     empresa : { 
@@ -9,7 +27,6 @@ const usuario ={
             número: 260
         }
     }
-    
 }
 
 console.log(`A empresa ${usuario.empresa.nome} está localizada na ${usuario.empresa.endereço.rua}, ${usuario.empresa.endereço.número}.`)
